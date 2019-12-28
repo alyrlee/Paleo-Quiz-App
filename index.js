3c53a3a014e478b56cbbd8eae48b4e1d0c3562f3
alyrlee <53842786+alyrlee@users.noreply.github.com>
2019-12-28 13:57:54 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/* when a user clicks on start quiz button */
+
+/* Displays question number and score obtained */
+
+/* Displays the options for the current question */
+
+//question database
+
+/* displays results and restart quiz button */
+
+/*checks whether the chosen option is right or wrong and displays respective message*/ 
