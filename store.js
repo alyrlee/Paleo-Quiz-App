ca1801abdb7db73120234ac7fe27a95bc9c7829a
alyrlee <53842786+alyrlee@users.noreply.github.com>
2019-12-30 20:19:10 -0500

    Update store.js

--- store.js
+++ store.js
@@ -1,1 +1,84 @@
+const STORE = {
+  questions: [
+    //1
+    {
+      question: "What is the Paleolithic?",
+      options: [
+        "The Paleolithic period is characterized from the first manufacture of stone tools to shortly before the development of agriculture",
+			  "The Paleolithic period occurred between 0.01 million years ago to 1.6 million years ago",
+			  "Pleistocene Period",
+			  "1,2,and 3 are correct"
+      ],
+      answer: "1,2,and 3 are correct"
+    },
+    //2
+    {
+      question: ["What is the Paleolithic Diet?"],
+      options: [
+        "Primarily wild game and vegetable foods",
+				"The Paleo-diet primarily had two food groups: meat/fish and vegetable/fish and did not include or rarely included grains",
+				"Protein contributed twice to five times that of the caloric intake for modern humans for Paleolithic humans and much more cholesterol and less fat",
+        "Gluten free, low-carb diet", "1,2,and 3 are correct"
+      ],
+      answer: "1,2,and 3 are correct"
+    },
+    //3
+    {
+      question: ["What are some benefits of the Paleolithic diet?"],
+      options: [
+        "Paleolithic diet allows for increase weight gain",
+				"Paleolithic diet allows for Flexibility and mobility",
+				"Paleolithic diet improves blood pressure, glucose tolerance, decreased insulin secretion, increased insulin sensitivity and improved lipid profiles",
+				"Does not guarantee you will lose weight, may have cardiovascular benefits, and may prevent or control diabetes"
+      ],
+      answer: "Paleolithic diet improves blood pressure, glucose tolerance, decreased insulin secretion, increased insulin sensitivity and improved lipid profiles"
+    },
+    //4
+    {
+      question: ["What are some drawbacks to the Paleolithic diet?"], 
+      options: [
+        "Not suited for modern human sedentary lifestyle",
+				"They are problematic as long-term subsistence strategies", 
+		    "Neglects that modern human diets reflects an omnivorous background with a tremendous diversity of diets suited to particularized environments",
+        "All of the above"
+        ],
+      answer: "All of the above"
+    },
+    //5
+    {
+      question: ["Who is the Paleolithic diet best suited for?"],
+      options: [
+        "Short term weight loss and fitness goals",
+				"Chronic health issues",
+				"Mostly anyone",
+				"No one"
 
+      ],
+      answer: "Mostly anyone"
+    },
+    //6
+      {
+      question: ["What types of foods are associated with the Paleolithic diet?"],
+      options: [
+      "Vegetables",
+			"Fruits and Nuts",
+			"Wild meats",
+			"1,2, and 3 are correct"
+      ],
+      answer: "1,2, and 3 are correct"
+    },
+    //7
+    {
+      question: ["What types of foods are not associated with the Paleolithic diet?"],
+      options: [
+      "Refined processed foods (containing refined oils such as shortening, margarine)",
+			"Sugars (sucrose, high fructose corn syrup, syrups)",
+			"Grains (whole grain, refined grains)",
+			"Dairy (milk, cheese, butter)","all of the above"
+      ],
+      answer: "all of the above"
+    }
+  ],
+  currentQuestion: 0,
+  score: 0
+};
